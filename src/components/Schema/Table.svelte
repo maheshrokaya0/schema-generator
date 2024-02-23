ac79724279b498a70534f8e762ea0ef83d11899b
Mr-Aaryan <kc.aryan3536@gmail.com>
2024-02-23 09:21:11 +0545

    Columns

--- src/components/Schema/Table.svelte
+++ src/components/Schema/Table.svelte
@@ -1,4 +1,6 @@
 <script>
+    import {onMount} from 'svelte'
+
     import Braces from '../.././assets/icons/braces.svelte'
     import Email from '../../assets/icons/email.svelte';
     import Calender from '../../assets/icons/calender.svelte';
@@ -8,6 +10,8 @@
     import Decimal from '../../assets/icons/decimal.svelte';
     import Text from '../../assets/icons/text.svelte';
     import Setting from '../../assets/icons/setting.svelte';
+    import Menu from '../../assets/icons/menu.svelte';
+    import Button from '../Site/button.svelte';
     
 
     const list = {
@@ -20,29 +24,63 @@
         Relation: Relation,
         Json: Braces
     };
-    let newArray = [{}];
+    let newArray = [];
     
-    let newCol, plusIcon, colItems, inputField, inputIcon;
+    let newCol, plusIcon, colItems, inputField, menu, menuItems, settingIcon, settings;
 
     function colfun(){
         newCol.classList.add("rounded-b-none");
         colItems.classList.remove("hidden");
-        
     }
+    let idCounter = 0;
     function colOptFun(key, Component){
         const obj = {
+            id: idCounter++,
             key,
-            Component
+            Component,
+            toggle: true
         };
         newArray = [...newArray, obj];
-        inputField.value = "Field";
-        inputField.select();
-        console.log(newArray);
-        // newArray.unshift(key);
-        // fieldInput.classList.remove("hidden");
+    }
+    let toggle = true;
+    onMount(() =>{
+        document.addEventListener("click", handleClick);
+    });
+    function handleClick(event) {
+        const clickedInsideMenu = menuItems.contains(event.target) || menu.contains(event.target);
+        
+        if (!clickedInsideMenu) {
+            menuItems.classList.add("hidden");
+            menu.classList.remove("bg-gray-200");
+        }
+    }
+    function menuFun(){
+        toggle = !toggle;
+        if(!toggle){
+            menuItems.classList.remove("hidden");
+            menu.classList.add("bg-gray-200");
+        }else{
+            menuItems.classList.add("hidden");
+            menu.classList.remove("bg-gray-200");
+        }
+    }
+    function settingFun(itemId){
+        // const item = newArray.find(item => item.id === itemId);
+        toggle = !toggle;
+        if(!toggle){
+            settings.classList.remove("hidden");
+            settingIcon.classList.add("bg-gray-200", "rounded-lg");
+        }else{
+            settings.classList.add("hidden");
+            settingIcon.classList.remove("bg-gray-200", "rounded-lg");
+        }
     }
 
-
+    // window.addEventListener("click", (e)=>{
+    //     menuItems.classList.add("hidden");
+    //     menu.classList.remove("bg-gray-200");
+    // });
+    
 
 </script>
 
@@ -57,12 +95,31 @@
         </div>
         <div class="mt-5">
             <h4 class="text-lg border-b pb-2">Columns</h4>
-            {#each newArray as item}
-                <div>
-                    <div class="flex justify-between items-center py-2 border rounded my-5 shadow-lg">
-                            <i class="mx-2" bind:this={inputIcon}>#</i>
-                            <input type="text" class="py-1 px-2 w-full mx-1 tracking-widest rounded focus:outline-none focus:bg-gray-200" bind:this={inputField}/>
-                            <i class="mx-2"><Setting /></i>
+            {#each newArray as item (item.id)}
+                <div class="my-5">
+                    <div class="flex justify-between items-center py-2 border rounded shadow-lg ">
+                            <i class="mx-2">
+                                {#if item.Component}
+                                    <svelte:component this={item.Component} />
+                                {/if}
+                            </i>
+                            <input type="text" class="py-1 px-2 w-full mx-1 tracking-widest rounded focus:outline-none focus:bg-gray-200" placeholder="Field" bind:this={inputField}/>
+                            <i class="mx-2 hover:outline hover:outline-1 hover:outline-gray-200 hover:rounded-lg" bind:this={settingIcon}><button class="px-2 py-1 " on:click={()=> settingFun(item.id)}><Setting /></button></i>
+                    </div>
+                    <div class="hidden" bind:this={settings}>
+                        <div class="flex justify-between items-center border py-2 shadow-md">
+                            <div class="flex py-3 px-2">
+                                <div class="bg-[#D9D9D9] px-3 py-2 rounded-lg font-semibold"><Button label="Not Empty" /></div>
+                                <div class="ml-5 bg-[#D9D9D9] px-3 py-2 rounded-lg font-semibold"><Button label="Is Unique" /></div>
+                            </div>
+                            <div class="flex relative menus">
+                                <div class="hidden right-10 absolute shadow-lg bg-white border px-3 py-2 rounded" bind:this={menuItems}>
+                                    <p class="w-full border-b px-2 py-1 hover:bg-gray-200 hover:cursor-pointer"><button on:click={()=> menuFun()} >Delete</button></p>
+                                    <p class="w-full px-2 py-1 hover:bg-gray-200 hover:cursor-pointer"><button on:click={()=> menuFun()} >Duplicate</button></p>
+                                </div>
+                                <button class="hover:cursor-pointer hover:bg-gray-200 rounded-full mr-3 py-1 px-2" bind:this={menu} on:click={()=> menuFun()} ><Menu /></button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             {/each}
@@ -73,7 +130,7 @@
                 <div class="hidden w-full border-2 border-t-0 rounded-t-none border-black text-center py-2 rounded" bind:this={colItems}>
                     <div class="mx-4 flex flex-row flex-wrap">
                         {#each Object.entries(list) as [key, Component]}
-                            <div class="text-left basis-1/3 hover:cursor-pointer hover:bg-gray-300 rounded"><button class="flex px-3 py-2 w-full" on:click={()=> colOptFun({key}, Component)}><i><Component/></i><span class="pl-2">{key}</span></button></div>
+                            <div class="text-left basis-1/3 hover:cursor-pointer hover:bg-gray-300 rounded"><button class="flex px-3 py-2 w-full" on:click={()=> colOptFun(key, Component)}><i><Component/></i><span class="pl-2">{key}</span></button></div>
                         {/each}
                     </div>
                 </div>
---
+++ src/components/Site/button.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { Toggle } from 'flowbite-svelte';
+
+  export let label;
+</script>
+
+<Toggle color="green">
+  <span class="text-lg">{label}</span>
+</Toggle>
