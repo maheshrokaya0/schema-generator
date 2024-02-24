ab2ec274a8a85f5b9f393c3803fdcabd50fbff55
maheshrokaya0 <maheshrokaya6647@gmail.com>
2024-02-24 11:08:15 +0545

    column issue fixed

---
+++ src/components/Schema/Column.svelte
@@ -0,0 +1,67 @@
+<script>
+      import { onMount } from "svelte";
+    import Setting from "../../assets/icons/setting.svelte";
+    import Menu from "../../assets/icons/menu.svelte";
+    import Button from "../Site/button.svelte";
+
+    export let colId;
+    let menu = colId;
+    
+    export let Icon;
+
+    let inputField, menuItems, settingIcon;
+
+    function menuFun() {
+    menu = !menu;
+
+  }
+
+  function settingFun() {
+    colId = !colId;
+  }
+
+  // onMount(() => {
+  //   document.body.addEventListener("click", handleClick);
+  //   function handleClick(event) {
+  //   menu = false;
+  // }
+  // });
+
+</script>
+
+<div class="mt-5">
+  <div class="flex justify-between items-center py-2 border rounded shadow-lg">
+    <i class="mx-2">
+      {#if Icon}
+      <svelte:component this={Icon} />
+      {/if}
+    </i>
+    <input type="text" class="py-1 px-2 w-full mx-1 tracking-widest rounded focus:outline-none focus:bg-gray-200" placeholder="Field" bind:this={inputField} />
+    <i class="mx-2 hover:outline hover:outline-1 hover:outline-gray-200 hover:rounded-lg" bind:this={settingIcon}>
+      <button class="px-2 py-1" on:click={settingFun}><Setting /></button>
+    </i>
+  </div>
+  <div class:hidden={colId}>
+    <div class="flex justify-between items-center border py-2 shadow-md">
+      <div class="flex py-3 px-2">
+        <div class="bg-[#D9D9D9] px-3 py-2 rounded-lg font-semibold">
+          <Button label="Not Empty" />
+        </div>
+        <div class="ml-5 bg-[#D9D9D9] px-3 py-2 rounded-lg font-semibold">
+          <Button label="Is Unique" />
+        </div>
+      </div>
+      <div class="flex relative menus">
+        <button class="hover:cursor-pointer hover:bg-gray-200 rounded-full mr-3 py-1 px-2" on:click={menuFun}><Menu /></button>
+        <div class="hidden right-10 absolute shadow-lg bg-white border px-3 py-2 rounded" class:hidden={!menu}>
+          <p class="w-full border-b px-2 py-1 hover:bg-gray-200 hover:cursor-pointer">
+            <button>Delete</button>
+          </p>
+          <p class="w-full px-2 py-1 hover:bg-gray-200 hover:cursor-pointer">
+            <button>Duplicate</button>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
--- src/components/Schema/Table.svelte
+++ src/components/Schema/Table.svelte
@@ -1,5 +1,4 @@
 <script>
-  import { onMount } from "svelte";
   import Braces from "../.././assets/icons/braces.svelte";
   import Email from "../../assets/icons/email.svelte";
   import Calender from "../../assets/icons/calender.svelte";
@@ -8,9 +7,7 @@
   import Relation from "../../assets/icons/relation.svelte";
   import Decimal from "../../assets/icons/decimal.svelte";
   import Text from "../../assets/icons/text.svelte";
-  import Setting from "../../assets/icons/setting.svelte";
-  import Menu from "../../assets/icons/menu.svelte";
-  import Button from "../Site/button.svelte";
+  import Column from "./Column.svelte";
 
   const list = {
     "Plain Text": Text,
@@ -24,7 +21,7 @@
   };
   let newArray = [];
 
-  let newCol, plusIcon, colItems, inputField, menu, menuItems, settingIcon, settings;
+  let newCol, plusIcon, colItems;
 
   function colfun() {
     newCol.classList.add("rounded-b-none");
@@ -43,50 +40,17 @@
     return result;
   }
 
-  function colOptFun(key, Component) {
+   function colOptFun(key, Component) {
     const obj = {
       id: generateUniqueId(),
       key,
-      Component,
-      toggle: true,
+      Component
     };
     newArray = [...newArray, obj];
   }
 
-  let toggle = true;
-  onMount(() => {
-    document.addEventListener("click", handleClick);
-  });
-  function handleClick(event) {
-    const clickedInsideMenu = menuItems.contains(event.target) || menu.contains(event.target);
 
-    if (!clickedInsideMenu) {
-      menuItems.classList.add("hidden");
-      menu.classList.remove("bg-gray-200");
-    }
-  }
 
-  function menuFun() {
-    toggle = !toggle;
-    if (!toggle) {
-      menuItems.classList.remove("hidden");
-      menu.classList.add("bg-gray-200");
-    } else {
-      menuItems.classList.add("hidden");
-      menu.classList.remove("bg-gray-200");
-    }
-  }
-
-  function settingFun(itemId) {
-    toggle = !toggle;
-    if (!toggle) {
-      settings.classList.remove("hidden");
-      settingIcon.classList.add("bg-gray-200", "rounded-lg");
-    } else {
-      settings.classList.add("hidden");
-      settingIcon.classList.remove("bg-gray-200", "rounded-lg");
-    }
-  }
 </script>
 
 <div class="max-w-2xl mx-auto rounded mt-16 px-5">
@@ -101,44 +65,9 @@
     <div class="mt-5">
       <h4 class="text-lg border-b pb-2">Columns</h4>
       {#each newArray as item (item.id)}
-      <div class="my-5">
-        <div class="flex justify-between items-center py-2 border rounded shadow-lg">
-          <i class="mx-2">
-            {#if item.Component}
-            <svelte:component this={item.Component} />
-            {/if}
-          </i>
-          <input type="text" class="py-1 px-2 w-full mx-1 tracking-widest rounded focus:outline-none focus:bg-gray-200" placeholder="Field" bind:this={inputField} />
-          <i class="mx-2 hover:outline hover:outline-1 hover:outline-gray-200 hover:rounded-lg" bind:this={settingIcon}>
-            <button class="px-2 py-1" on:click={() => settingFun(item.id)}>(Id: {item.id})<Setting /></button>
-          </i>
-        </div>
-        <div class="hidden" bind:this={settings}>
-          <div class="flex justify-between items-center border py-2 shadow-md">
-            <div class="flex py-3 px-2">
-              <div class="bg-[#D9D9D9] px-3 py-2 rounded-lg font-semibold">
-                <Button label="Not Empty" />
-              </div>
-              <div class="ml-5 bg-[#D9D9D9] px-3 py-2 rounded-lg font-semibold">
-                <Button label="Is Unique" />
-              </div>
-            </div>
-            <div class="flex relative menus">
-              <div class="hidden right-10 absolute shadow-lg bg-white border px-3 py-2 rounded" bind:this={menuItems}>
-                <p class="w-full border-b px-2 py-1 hover:bg-gray-200 hover:cursor-pointer">
-                  <button on:click={() => menuFun()}>Delete</button>
-                </p>
-                <p class="w-full px-2 py-1 hover:bg-gray-200 hover:cursor-pointer">
-                  <button on:click={() => menuFun()}>Duplicate</button>
-                </p>
-              </div>
-              <button class="hover:cursor-pointer hover:bg-gray-200 rounded-full mr-3 py-1 px-2" bind:this={menu} on:click={() => menuFun()}><Menu /></button>
-            </div>
-          </div>
-        </div>
-      </div>
+        <Column colId ={item.id} Icon = {item.Component}/>
       {/each}
-      <div>
+      <div class="mt-5">
         <button class="block w-full border-2 border-black py-2 rounded hover:bg-gray-200 text-sm font-semibold hover:cursor-pointer" bind:this={newCol} on:click={() => colfun()}><span bind:this={plusIcon}>+</span> New column</button>
       </div>
       <div>
